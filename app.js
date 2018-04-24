fbcc1c80c581775a1e08b9ff85d8383a7dc1eee7
Jack DiSalvatore <jdisalvatore7@gmail.com>
2018-04-24 06:00:43 -0400

    Creating the first page

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
-   res.send("<h1> Hello World! </h1>");
+app.get('/', function(req, res) {
+   res.render('index');
 });
 
 app.listen(3000 , function( ) {
